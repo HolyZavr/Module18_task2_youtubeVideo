0d104065bbcef706752aaef7c35e43ebfef58f3b
Name <email@example.ru>
2021-12-20 00:16:27 +1100

    Осталось только паттерн добить...

---
+++ Module18_task2_youtubeVideo/Operations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module18_task2_youtubeVideo
+{
+    abstract class Operations
+    {
+        abstract public void GetInformation();
+        abstract public void DownloadVideo();
+    }
+}
